5124da9d911f7349f9d6ffd24fb1e94bed4eb563
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2025-11-18 11:46:24 +0530

    Conversion Operation of js

---
+++ JavaScript/ConOper.js
@@ -0,0 +1,47 @@
+
+let score = "hitesh"
+console.log(typeof score); //console.log(typeof(score));
+
+let valInNum = Number(score)
+console.log(typeof valInNum); // number
+console.log(valInNum); //NaN
+
+let IsloggedIn = "hitesh"
+
+let booleanIsloggedIn = Boolean(IsloggedIn )
+console.log(booleanIsloggedIn); // true
+
+
+let value = 5
+let negvalue = -value
+console.log(negvalue);// -5
+
+console.log(2+2); // 4
+console.log(2-2); // 0
+console.log(2*2); // 4
+console.log(2**3); // 8
+console.log(2/3); //0.66666
+console.log(2%4); //2
+
+
+console.log("1" + 2); //12
+console.log( 1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log((3+4) *5%3);
+
+
+
+let num = 100;
+num++;
+console.log(num);
+
+
+let str1 = "Vaibhav";
+let str2 = " Reddy";
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log(+ true);
+console.log(+ "");
