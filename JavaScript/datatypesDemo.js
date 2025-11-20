0595b0b747c2e72ec5eb7800f455ab461b657aa5
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2025-11-20 12:12:57 +0530

    datatypes

---
+++ JavaScript/datatypesDemo.js
@@ -0,0 +1,19 @@
+// dataTypes
+
+const Score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail;
+
+console.table([isLoggedIn,outsideTemp,scorevalue,Score]);
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); // false
+
+const bigNumber = 3456543576654356754;
+
+
