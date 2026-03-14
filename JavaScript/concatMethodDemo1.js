74d49c54a13cc970306c8de863cc5ed95f92d3c9
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-03-14 15:36:40 +0530

    first commit

---
+++ JavaScript/concatMethodDemo1.js
@@ -0,0 +1,9 @@
+let a = [10,20];
+let b = [30,40];
+let c = [40,50];
+console.log("set a : " + a);
+console.log("set b : " + b);
+console.log("set c : " + c);
+let d = a.concat(b,c);
+
+console.log("Merge all Arrays: " + d);
---
+++ JavaScript/isArrayMethodDemo1.js
@@ -0,0 +1,11 @@
+let a = [10,20,30,40];
+console.log("Before applying isArray Method : " + a );
+let res = Array.isArray(a);
+console.log("result: " + res);
+
+console.log();
+
+let b = 10;
+console.log("Before applying isArray Method : " + b );
+let res1 = Array.isArray(b);
+console.log("result: " + res1);
