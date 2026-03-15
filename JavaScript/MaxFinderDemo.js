bbf062e42a283f6087b83d45453bf01256d88c0f
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-03-15 12:09:10 +0530

    successfull

---
+++ JavaScript/MaxFinderDemo.js
@@ -0,0 +1,7 @@
+//for maxfinder
+
+let a = [10,20,30,40,50,506,90];
+
+let b = Math.max.apply(null,a);
+
+console.log("Maximum element of given array : " + b);
---
+++ JavaScript/MinFinderDemo.js
@@ -0,0 +1,6 @@
+//for minFinder
+
+let a = [100,400,200,600,"900"];
+
+let b = Math.min.apply(null,a);
+console.log("Minimum element of given array: " + b);
