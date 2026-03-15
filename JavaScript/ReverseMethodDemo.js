6a4b13da6590e919b79ce0283e92caa14f008434
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-03-15 11:34:57 +0530

    several commits

---
+++ JavaScript/ReverseMethodDemo.js
@@ -0,0 +1,7 @@
+// reverse method 
+
+let a = [10,20,30,40,"good"];
+console.log("Before Reversing array " + a);
+
+let res = a.reverse();
+console.log("After Reversing array " + a);
---
+++ JavaScript/UniqueSortDemo.js
@@ -0,0 +1,11 @@
+// sort array with descendig order then we use both sort() and Array()
+
+let a = [10,20,30,40,90,80,70,12];
+console.log("before Sort: " + a);
+
+a.sort();
+a.reverse();
+
+console.log("After sort in descending : " + a);
+
+
