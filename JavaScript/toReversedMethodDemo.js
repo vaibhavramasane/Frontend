6c57c45b1ca65f70981de5e070c2a3db084f8172
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-03-15 12:02:31 +0530

    commit successful

---
+++ JavaScript/toReversedMethodDemo.js
@@ -0,0 +1,6 @@
+//toReversed
+
+let a = [9,8,7,6,5,4];
+let b = a.toReversed();
+console.log("Original Array : " + a);
+console.log("After Reversed : " + b);
---
+++ JavaScript/tosortedMethodeDemo.js
@@ -0,0 +1,7 @@
+// tosorted
+
+let a = [1,3,9,5,7];
+
+let newA = a.toSorted();
+console.log("Original Array : " +a);
+console.log("Array after sorted " + newA);
