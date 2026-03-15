a6cc194ec4f38a30e5ded9667876fc4bfc495937
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-03-15 12:23:40 +0530

    demo

---
+++ JavaScript/ForEachDemo1.js
@@ -0,0 +1,9 @@
+let a = [10,20,30,40,50];
+
+a.forEach(myfun);
+
+function myfun(value,index,array){
+	
+	console.log(value);
+	
+}
---
+++ JavaScript/LambdaExpDemo1.js
@@ -0,0 +1,5 @@
+//lambda expression
+
+let a  = [10,20,304,05,50,60];
+
+a.forEach((val) => console.log(val));
