dba688ac67abb76fb1ca38023de0eb9d1a5222a5
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-03-16 11:04:39 +0530

    first commit

---
+++ JavaScript/StringLenMethodDemo1.js
@@ -0,0 +1,4 @@
+//String method length
+let str = "Good";
+let len = str.length;
+console.log("Length of String : " + len);
---
+++ JavaScript/charAtMethodDemo1.js
@@ -0,0 +1,9 @@
+//CharAt() method
+let str = "Good";
+for(let i=0; i<str.length; i++){
+
+	let ch = str.charAt(i);
+	console.log(i+ "----->" +ch);
+	
+}
+	
