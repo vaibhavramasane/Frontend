ea78717b202b0b3ea76c5e90818bb1a289bd7b1a
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-03-16 11:12:05 +0530

    first commit

---
+++ JavaScript/StrconcatMethodDemo1.js
@@ -0,0 +1,5 @@
+//concat
+let first = "Good ";
+let sec = "Morning ";
+let res = first.concat(sec ," pqr "," Stv ");
+console.log(res);
---
+++ JavaScript/charCodeAtMethodDemo1.js
@@ -0,0 +1,9 @@
+//charCodeAt()
+
+let str = "Vaibhav";
+for(let i=0; i<str.length; i++){
+
+	let ch = str.charCodeAt(i);
+	console.log(i+ "-----> "+ ch);
+	
+}
