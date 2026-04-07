d1c6b41178fb4fadabcf946a6690793bf1ce5732
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-04-07 15:34:12 +0530

    first

---
+++ JavaScript/if-else/Q5.js
@@ -0,0 +1,16 @@
+/* 5. Vowel or Consonant 
+Logic: 
+Check if character is a, e, i, o, u (case-insensitive). 
+Example: 
+Input: e 
+Output: Vowel */
+
+let ch = 'e';
+if(ch=='a'|| ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+	
+	console.log("Vowel");
+}
+else{
+	console.log("Consonant");
+	
+}
