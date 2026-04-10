9035015cb18157985cb3f0a08c6c2baac5ce1599
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-04-10 17:59:32 +0530

    final commit

---
+++ JavaScript/Array/Q7.js
@@ -0,0 +1,7 @@
+/*Q7. Sort Numbers
+Problem: Sort [5,2,9,1] in ascending order using .sort().
+*/
+
+let nums = [5,2,9,1];
+nums.sort((a,b)=>a-b);
+console.log(nums);
---
+++ JavaScript/Array/Q8.js
@@ -0,0 +1,8 @@
+/*Q8. Join Array
+Problem: Join ['a','b','c'] into a 
+string with - separator
+ using .join() */
+ 
+let nums = ['a','b','c'];
+let result = nums.join();
+console.log(result);
