51f9fc6f2cf8dc469ce96009c352e79d2a259178
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-04-10 18:07:20 +0530

    final commit

---
+++ JavaScript/Array/Q10.js
@@ -0,0 +1,9 @@
+/*Q10. Remove First Element
+Problem: Remove first element
+ of [10,20,30] using .shift(). */
+ 
+let nums = [10,20,30];
+
+let removed = nums.shift();
+
+console.log(nums);
---
+++ JavaScript/Array/Q9.js
@@ -0,0 +1,8 @@
+/*Q9. Add Elements
+Problem: Add 6,7 to array [1,2,3,4,5]
+ using .push(). */
+ 
+ let nums = [1,2,3,4,5];
+ nums.push(6,7);
+ console.log(nums);
+ 
