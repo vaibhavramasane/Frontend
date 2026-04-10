9ca13ee3c8f6f409364f7b415c27b6c69f4e3696
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-04-10 17:36:41 +0530

    sec commit

---
+++ JavaScript/Array/Q3.js
@@ -0,0 +1,5 @@
+/*Q3. Map to Square 
+Problem: Square each number in [1,2,3,4] using .map(). */
+let n = [1,2,3,4,5,6];
+let square = n.map(n=> n*n);
+console.log(square);
---
+++ JavaScript/Array/Q4.js
@@ -0,0 +1,7 @@
+/*Q4. Find a Number 
+Problem: Find first number greater 
+than 10 in [5,12,8,20] using .find(). */
+
+let nums = [5,12,8,20];
+let findsnum = nums.find(nums=>nums>10);
+console.log(findsnum);
